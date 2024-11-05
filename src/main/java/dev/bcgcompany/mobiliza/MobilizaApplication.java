21300eea31f6278c2569e3b1c01ea340184cb7b1
nicuis <bkscarpa@gmail.com>
2024-11-04 22:02:11 -0300

    Criação do projeto

---
+++ src/main/java/dev/bcgcompany/mobiliza/MobilizaApplication.java
@@ -0,0 +1,13 @@
+package dev.bcgcompany.mobiliza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bcgcompany/mobiliza/MobilizaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bcgcompany.mobiliza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
