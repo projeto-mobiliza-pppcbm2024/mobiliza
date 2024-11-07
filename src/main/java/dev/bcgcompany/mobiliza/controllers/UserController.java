d6c71d2622366d37db03738789b67cea74e5badb
nicolas scarpa <nicolas.scarpa@fundace.org.br>
2024-11-07 16:14:05 -0300

    Criação da classe UserController

---
+++ src/main/java/dev/bcgcompany/mobiliza/controllers/UserController.java
@@ -0,0 +1,22 @@
+package dev.bcgcompany.mobiliza.controllers;
+
+import dev.bcgcompany.mobiliza.dto.CadastroUsuarioRequestDTO;
+import dev.bcgcompany.mobiliza.dto.UsuarioCadastradoResponseDTO;
+import dev.bcgcompany.mobiliza.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioCadastradoResponseDTO> registerUser(@RequestBody CadastroUsuarioRequestDTO cadastroUsuarioRequestDTO) {
+        UsuarioCadastradoResponseDTO responseDTO = userService.registerUser(cadastroUsuarioRequestDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+}
